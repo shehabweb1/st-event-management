ec29c8e1a42ef6feff302f8d2e4722065fe5fd08
MD Shehab Hossen <shehab.web1@gmail.com>
2023-10-08 14:27:24 +0600

    Installation Done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { auth } from './firebase/firebase.config';
+
+const App = () => {
+  console.log(auth)
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default App;
